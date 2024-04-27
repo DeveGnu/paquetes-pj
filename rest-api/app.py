a716cf146849792c7e821d2fafc18dceec63fab6
Marian_O <oromerom@guanajuato.gob.mx>
2024-04-27 09:01:44 -0600

    Add first  endpoints estructure

--- rest-api/app.py
+++ rest-api/app.py
@@ -1,20 +1,55 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from markupsafe import escape
 
 app_flask = Flask(__name__)
 
-@app_flask.route('/ping')
+# GET
+@app_flask.route('/ping',methods=['GET'])
 def ping():
-    
+    example_test = request.args.get('usuario')
     return jsonify({
         "status":True,
         "data":{
-            "message": 'pong'
+            "message": example_test
         }
     })
     
+# POST
+@app_flask.route('/example_post',methods=['POST'])
+def example_post():
+    data = request.json
+    return jsonify({
+        "status":True,
+        "data":{
+            "message": [data['id'],data['item']]
+        }
+    })
+    
+#PUT
+@app_flask.route('/example_put/<int:id>/<string:name>',methods=['PUT'])
+def example_put(id,name):
+    return jsonify({
+        "status":True,
+        "data":{
+            "message": {
+                "id":id,
+                "name":name
+            }
+        }
+    })
+    
+#DELETE
+@app_flask.route('/example_delete/<int:id>',methods=['DELETE'])
+def example_delete(id):
+    return jsonify({
+        "status":True,
+        "data":{
+            "message": 'Item deleted succefully'
+        }
+    })
+
+
 
 #entry point 
 if __name__ == '__main__':
--- rest-api/prueba.py
+++ rest-api/prueba.py
@@ -1,3 +1,19 @@
+import time
+
+def meture_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Tiempo: {func.__name__}: {end - start} segundos")
+        return result
+    return wrapper
+
+@meture_execution_time
 def testing():
     print('Funcion de prueba')
-    
+    print('Funcion de prueba')
+    print('Funcion de prueba')
+    print('Funcion de prueba')
+
+
